2f2743e5c2afeac1bb554c2821e290a78345ca7d
Oleh_Babiak <oleh_babiak@epam.com>
2025-01-14 15:21:37 +0200

    Course finished

--- src/app/action.js
+++ src/app/action.js
@@ -1,8 +1,10 @@
 "use server";
 
 import { openai } from '@ai-sdk/openai';
-import { generateText, streamText } from 'ai';
-import { createStreamableValue } from 'ai/rsc';
+import { generateText, streamText, generateObject, streamObject } from 'ai';
+
+import { createStreamableValue, streamUI } from 'ai/rsc';
+import { object, z } from 'zod'
 
 export async function generate(input) {
     const stream = createStreamableValue("");
@@ -26,4 +28,56 @@ export async function getAnswer(prompt) {
     });
 
     return { text };
+}
+
+export async function getData(input) {
+    'use server';
+    const { object: people } = await generateObject({
+        model: openai('gpt-3.5-turbo'),
+        system: "You generate data for three people.",
+        prompt: input,
+        schema: z.object({
+            people: z.array(
+                z.object({
+                    name: z.string().describe("The name of the fake person."),
+                    address: z.string().describe("Us address format."),
+                    age: z.number(),
+                })),
+        }),
+    });
+    return { people };
+};
+
+export async function generateObj(input) {
+    'use server';
+    const stream = createStreamableValue();
+    (async () => {
+        const { partialObjectStream } = await streamObject({
+            model: openai('gpt-3.5-turbo'),
+            system: "You generate data for three people.",
+            prompt: input,
+            schema: z.object({
+                people: z.array(
+                    z.object({
+                        name: z.string().describe("The name of the fake person."),
+                        address: z.string().describe("Us address format."),
+                        age: z.number(),
+                    })),
+            }),
+        });
+        for await (const partialObject of partialObjectStream) {
+            stream.update(partialObject);
+        }
+        stream.done();
+    })();
+    return { object: stream.value };
+};
+
+export async function streamComponent() {
+    const result = await streamUI({
+        model: openai('gpt-4o'),
+        prompt: "Give me some advice on how to plan a trip to San Francisco.",
+        text: ({ content }) => <div>{content}</div>
+    });
+    return result.value;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import { useChat } from "ai/react";
 
 import { useState } from "react"
-import { getAnswer, generate } from "./action";
+import { getAnswer, generate, getData, generateObj, streamComponent } from "./action";
 import { readStreamableValue } from 'ai/rsc';
 
 
@@ -37,13 +37,20 @@ export const maxDuration = 30;
 export default function Home() {
   const [generation1, setGeneration1] = useState("");
   const [generation2, setGeneration2] = useState("");
+  const [component, setComponent] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-full justify-between w-60">
+      {/* <div className="flex w-full justify-between w-60">
         <button onClick={async () => {
           const { text } = await getAnswer("What should I do in San Francisco?");
           setGeneration1(text);
         }}>Ask</button>
+        <button onClick={
+          async () => {
+            const { people } = await getData("People who sound like they have superhero names");
+            setGeneration1(JSON.stringify(people, null, 2));
+          }
+        }>View People</button>
         <button onClick={async () => {
           const { output } = await generate("What should I do in San Francisco?");
 
@@ -51,10 +58,30 @@ export default function Home() {
             setGeneration2(currentGeneration => currentGeneration + delta);
           }
         }}>Ask2</button>
+
+        <button onClick={
+          async () => {
+            const { object } = await generateObj("People who sound like they have superhero names");
+            for await (const partialObject of readStreamableValue(object)) {
+              if (partialObject) {
+                setGeneration2(JSON.stringify(partialObject.people, null, 2));
+              }
+            }
+          }
+        }>View People2</button>
+      </div> */}
+      <div className="flex w-full flex-col justify-between w-60">
+        <form onSubmit={async (e) => {
+          e.preventDefault();
+          setComponent(await streamComponent());
+        }}>
+          <button >Get component</button>
+        </form>
+        <div>{component}</div>
       </div>
       <div className="flex w-full justify-between w-60">
-        <p>{generation1}</p>
-        <p>{generation2}</p>
+        <pre>{generation1}</pre>
+        <pre>{generation2}</pre>
       </div>
     </main>
   );
